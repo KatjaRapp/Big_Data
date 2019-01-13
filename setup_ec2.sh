71263294d36dc5997b7c95d5915249ec751f6210
Markus-Heidemann <marheidemann@googlemail.com>
2019-01-13 13:44:56 +0100

    change data permissions

--- setup_ec2.sh
+++ setup_ec2.sh
@@ -11,3 +11,4 @@ apt-get install unzip -y
 mkdir -p /home/ubuntu/data
 python3 ./google_drive.py "16N-AWvkcvhtQbLT7irpvSHSozJtyvGel" /home/ubuntu/data/stocks.zip
 unzip /home/ubuntu/data/stocks.zip -d /home/ubuntu/data
+chmod 666 /home/ubuntu/data
